e97217c155510760ec8453074e1e0526ebeac2e8
あさぴっぴ1020 <88693257+Asapi1020@users.noreply.github.com>
2024-10-21 08:11:06 +0900

    🎉 YouTube Announcer API

---
+++ api/channels/videos/index.ts
@@ -0,0 +1,47 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import {
+	getChannelDetails,
+	getFeeds,
+	getNewVideoIDs,
+	getVideoDetails,
+	postVideos,
+	sortVideos,
+} from "../../../src/utils";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+	const { channelID, lastUpdate } = req.query;
+
+	if (!channelID) {
+		return res.status(400).json({ error: "Invalid channelId" });
+	}
+
+	try {
+		const channelIDs = typeof channelID === "string" ? [channelID] : channelID;
+		const feeds = await getFeeds(channelIDs);
+		const videoIDs = getNewVideoIDs(
+			feeds,
+			lastUpdate && typeof lastUpdate === "string" ? lastUpdate : undefined,
+		);
+		const videos = await getVideoDetails(videoIDs);
+		const sortedVideos = sortVideos(videos);
+		const channels = await getChannelDetails(channelIDs);
+
+		for (const video of sortedVideos) {
+			const authorChannel = channels.find(
+				(channel) => channel.id === video.snippet?.channelId,
+			);
+			if (authorChannel) {
+				await postVideos(video, authorChannel);
+			} else {
+				console.error("Failed to find author channel");
+			}
+		}
+
+		const lastPublishedAt =
+			sortVideos[sortVideos.length - 1].snippet.publishedAt;
+		res.status(200).json({ lastUpdate: lastPublishedAt });
+	} catch (error) {
+		console.error("Error fetching RSS feed:", error);
+		res.status(500).json(error);
+	}
+}
---
+++ src/utils.ts
@@ -0,0 +1,135 @@
+import { google, type youtube_v3 } from "googleapis";
+import Parser from "rss-parser";
+import type { DiscordWebhookPayload, Feed } from "./type";
+
+export const getFeed = async (channelID: string): Promise<Feed> => {
+	const rssUrl = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelID}`;
+	const parser = new Parser();
+	const feed = await parser.parseURL(rssUrl);
+	return feed;
+};
+
+export const getFeeds = async (channelIDs: string[]): Promise<Feed[]> => {
+	const feedPromises = channelIDs.map((channelID) => getFeed(channelID));
+	const feeds = await Promise.all(feedPromises);
+	return feeds;
+};
+
+export const getNewVideoIDs = (
+	feeds: Feed[],
+	lastUpdate: string | undefined,
+): string[] => {
+	const newFeeds = feeds.map((feed) => {
+		return lastUpdate
+			? feed.items.filter((item) => {
+					const publishedDate = new Date(item.pubDate as string);
+					return publishedDate > new Date(lastUpdate);
+				})
+			: feed.items;
+	});
+
+	const videoIDs = newFeeds.flatMap((feed) => {
+		return feed.map((item) => {
+			const splitID = item.id.split("yt:video:");
+			return splitID.length > 1 ? splitID[1] : "";
+		});
+	});
+
+	return videoIDs;
+};
+
+export const sortVideos = (
+	videos: youtube_v3.Schema$Video[],
+): youtube_v3.Schema$Video[] => {
+	return videos.sort((a, b) => {
+		const dateA = new Date(a.snippet.publishedAt).getTime();
+		const dateB = new Date(b.snippet.publishedAt).getTime();
+		return dateA - dateB;
+	});
+};
+
+export const getVideoDetails = async (
+	videoIDs: string[],
+): Promise<youtube_v3.Schema$Video[]> => {
+	const youtube = google.youtube({
+		version: "v3",
+		auth: process.env.YOUTUBE_API_KEY,
+	});
+
+	const response = await youtube.videos.list({
+		part: ["snippet", "status", "liveStreamingDetails"],
+		id: videoIDs,
+	});
+
+	if (!response.data.items || response.data.items.length === 0) {
+		throw new Error("Not Found");
+	}
+
+	return response.data.items;
+};
+
+export const getChannelDetails = async (
+	channelIDs: string[],
+): Promise<youtube_v3.Schema$Channel[]> => {
+	const youtube = google.youtube({
+		version: "v3",
+		auth: process.env.YOUTUBE_API_KEY,
+	});
+
+	const response = await youtube.channels.list({
+		part: ["snippet"],
+		id: channelIDs,
+	});
+
+	if (!response.data.items || response.data.items.length === 0) {
+		throw new Error("Not Found");
+	}
+
+	return response.data.items;
+};
+
+export const postVideos = async (
+	video: youtube_v3.Schema$Video,
+	channel: youtube_v3.Schema$Channel,
+): Promise<void> => {
+	const url = process.env.WEBHOOK_URL;
+	if (!url) {
+		throw new Error("Internal Server Error: Undefined Webhook URL");
+	}
+
+	const payload: DiscordWebhookPayload = {
+		embeds: [
+			{
+				author: {
+					name: video.snippet?.channelTitle,
+					url: channel.id
+						? `https://www.youtube.com/channel/${channel.id}`
+						: undefined,
+					icon_url: channel.snippet?.thumbnails?.default?.url,
+				},
+				title: video.snippet?.title,
+				url: video.id
+					? `https://www.youtube.com/watch?v=${video.id}`
+					: undefined,
+				description: video.snippet?.description,
+				image: {
+					url: video.snippet?.thumbnails?.default?.url,
+				},
+				color: 0xff0000,
+				timestamp: video.snippet?.publishedAt,
+			},
+		],
+	};
+
+	const response = await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(payload),
+	});
+
+	if (!response.ok) {
+		throw new Error("Failed to post video");
+	}
+};
